63db7fffd749ce66b1718512f03738fd849e1e11
Nicocaci <nicko.caci@gmail.com>
2025-07-18 17:17:59 -0300

    push

--- src/dao/models/order-model.js
+++ src/dao/models/order-model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     nombre: String,
+    telefono: Number,
     email: String,
     direccion: String,
     productos: [
