0996a16bb51ee606dfabcafacb2eff3b607bf6a5
Nicocaci <nicko.caci@gmail.com>
2025-09-22 10:57:45 -0300

    push

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
-            //"https://www.digitalshopok.com",
+    origin: //"http://localhost:5173",
+            "https://www.digitalshopok.com",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
